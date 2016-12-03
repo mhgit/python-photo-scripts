fd56cfc1af21c74adc5a76ec62f7424cb30190b8
Martin <martin.harris@tek-design.com>
2016-12-03 23:33:09 +0000

    Add system to validate the archive.  Plus other improvements including type checking and docs.

--- nas-archiving/create-glacier-archive.py
+++ nas-archiving/create-glacier-archive.py
@@ -6,12 +6,8 @@
 # the final archive will be /share/backup-jobs/aws-glacier/backup_store0035/store0035.tar.bz2
 #
 # todo list
-# Validate the file entries.
-# Validate the files against the originals.
 # Create md5 file for tar
 # Flag to compare md5 against original file.
-# http://stackoverflow.com/questions/2018512/reading-tar-file-contents-without-untarring-it-in-python-script
-# http://stackoverflow.com/questions/254350/in-python-is-there-a-concise-way-of-comparing-whether-the-contents-of-two-text
 # http://stackoverflow.com/questions/3431825/generating-an-md5-checksum-of-a-file
 
 
@@ -26,17 +22,24 @@ IGNORE_PATTERNS = ('*.DS_Store', '*.@__thumb')
 _dir_to_default = "/share/backup-jobs/aws-glacier"
 _dir_to_prefix = "backup_"
 
-_ignored_files = set()
-_included_files = set()
-
 
 class FileStatus(object):
     def __init__(self):
+        """
+        Collect the reasons why files are skipped
+        """
         pass
 
 
 class Flags(object):
     def __init__(self, verbose=False, summary=False, list_only=False, check_contents=False):
+        """
+        Collect args from the command line.
+        :param verbose: Print progress and other notes.
+        :param summary: Print summary information.
+        :param list_only: Only list intentions.
+        :param check_contents: Check the contents of an existing archive against the filesystem.
+        """
         self.verbose = verbose
         self.summary = summary
         self.list_only = list_only
@@ -51,15 +54,15 @@ def print_help():
     print ''
     print 'Creates a tar cleaned of all files we do not want to send to offline archive.'
     print 'Ignores symbolic links'
-    print 'Ignored names: [{}]'.format(', '.join(IGNORE_PATTERNS)).join('\n')
+    print 'Ignored patterns: [{}]'.format(IGNORE_PATTERNS)
 
-    print 'Typical use cases:\n'
+    print '\nTypical use cases:\n'
 
     print 'Report a dry run:'
     print sys.argv[0] + ' -d <input directory> -l\n'
 
     print 'Report summary:'
-    print ''.join(sys.argv[0]) + (' -i <input directory> -s\n')
+    print sys.argv[0] + (' -i <input directory> -s\n')
 
     print 'Creating archives:'
     print 'With no output the default location is used: [{}]'.format(_dir_to_default)
@@ -72,11 +75,19 @@ def print_help():
     print 'Checking archives and validating the contents:'
 
     print sys.argv[0] + ' -i <input directory> -o <output directory> -c\n'
+    return
 
 
-#
-# Collect skipped file
 def add_skip_file(ignored_files, src, name, why):
+    # type: (set, str, str, str) -> None
+
+    """
+    Collect skipped files
+    :param ignored_files:
+    :param src:
+    :param name:
+    :param why:
+    """
     file_status = FileStatus()
     file_status.why = why
     file_status.fileName = os.path.join(src, name)
@@ -85,9 +96,14 @@ def add_skip_file(ignored_files, src, name, why):
     return
 
 
-#
-# Find files
-def list_tree(src, ignore=None):
+def list_tree(src, included_files, skipped_files, ignore=None):
+    """
+    Walk the tree and find all files.  Populates ignored_files and included_files
+    :param src: Route folder name.
+    :param skipped_files: Collect the skipped files
+    :param included_files: Collect the included files
+    :param ignore: Function for patterns to ignore.
+    """
     names = os.listdir(src)
 
     if ignore is not None:
@@ -98,22 +114,22 @@ def list_tree(src, ignore=None):
     errors = []
     for name in names:
         if name in ignored_names:
-            add_skip_file(_ignored_files, src, name, 'skip pattern match')
+            add_skip_file(skipped_files, src, name, 'skip pattern match')
             continue
 
         srcname = os.path.join(src, name)
         try:
             if os.path.isdir(srcname) and not os.path.islink(srcname):
-                list_tree(srcname, ignore)
+                list_tree(srcname, included_files, skipped_files, ignore)
             elif os.path.islink(srcname):
-                add_skip_file(_ignored_files, src, name, 'skip symbolic link')
+                add_skip_file(skipped_files, src, name, 'skip symbolic link')
             else:
-                _included_files.add(srcname)
+                included_files.add(srcname)
 
         # What about devices, sockets etc.?
         except (IOError, os.error) as why:
             errors.append((srcname, str(why)))
-        # catch the Error from the recursive listtree so that we can
+        # catch the Error from the recursive tree so that we can
         # continue with other files
         except EnvironmentError as err:
             errors.extend(err.args[0])
@@ -124,9 +140,13 @@ def list_tree(src, ignore=None):
     return
 
 
-#
-# Print files which will be [added, not added] to archive
-def print_list(included_files, ignored_files):
+def print_list(included_files, skipped_files):
+    # type: (set, set) -> None
+    """
+    Print files which will be [added, not added] to archive
+    :param included_files:
+    :param skipped_files:
+    """
     print '\n** List files only! **'
     print '\nIgnore Names: [{}]'.format(', '.join(IGNORE_PATTERNS))
 
@@ -136,25 +156,35 @@ def print_list(included_files, ignored_files):
 
     print ''
     print '--Ignored--'
-    for file_status in sorted(ignored_files):
+    for file_status in sorted(skipped_files):
         print '[{}],[{}]'.format(file_status.why, file_status.fileName)
     return
 
 
-#
-# Print a sum totals.
-def print_summary(included_files, ignored_files):
+def print_summary(included_files, skipped_files):
+    # type: (set, set) -> None
+    """
+    Print a sum totals.
+    :param included_files:
+    :param skipped_files:
+    """
     print ''
     print '--Summary--'
     print '{} included files.'.format(len(included_files))
-    print '{} ignored files.\n'.format(len(ignored_files))
+    print '{} ignored files.\n'.format(len(skipped_files))
     return
 
 
-#
-# Create a new location for the file based on its name
-def create_to_dir(dir_to, dir_to_prefix, to_dir_path_end):
-    create_dir = os.path.join(dir_to, dir_to_prefix + to_dir_path_end)
+def create_to_dir(dir_to, dir_to_prefix, dir_to_suffix):
+    # type: (str, str, str) -> str
+    """
+    Create a new location for the file based on its name
+    :param dir_to: Folder to write too.
+    :param dir_to_prefix: Prefix for the folder. i.e backup_
+    :param dir_to_suffix: Suffix for the folder. i.e store0035
+    :return: folder name where the tar will be sent
+    """
+    create_dir = os.path.join(dir_to, dir_to_prefix + dir_to_suffix)
 
     if not os.path.exists(create_dir):
         os.mkdir(create_dir, 0755)
@@ -162,27 +192,37 @@ def create_to_dir(dir_to, dir_to_prefix, to_dir_path_end):
     return create_dir
 
 
-#
-# Build the compressed archive
 def create_archive(tar_filename, included_files, flags):
+    # type: (str, set, Flags) -> None
+    """
+
+    :param tar_filename:
+    :param included_files:
+    :param flags:
+    """
     i = len(included_files)
 
     if os.path.isfile(tar_filename):
-        raise Exception("Tar exists! Will not overwrite, user must remove. " + tar_filename)
+        sys.exit("Tar exists! Will not overwrite, user must remove. [{}]".format(tar_filename))
 
     with tarfile.open(tar_filename, "w:bz2") as tar:
         for name in included_files:
             if flags.verbose:
-                print 'a[{}] {}'.format(i, name)
+                print 'a [{}] {}'.format(i, name)
             tar.add(name, arcname=os.path.abspath(name))
             i -= 1
 
     return
 
 
-#
-# Create the folder where the tar will be written and return the name.
 def build_tar_location(dir_from, dir_to):
+    # type: (str, str) -> str
+    """
+    Create the folder where the tar will be written and return the name.
+    :param dir_from: Where to find the files.
+    :param dir_to: Where to send the files.
+    :return: The name of the tar being created.
+    """
     to_dir_path_end = os.path.basename(os.path.normpath(dir_from))
     archive_to_dir = create_to_dir(dir_to, _dir_to_prefix, to_dir_path_end)
     tar_filename = archive_to_dir + '/' + to_dir_path_end + '.tar.bz2'
@@ -190,37 +230,92 @@ def build_tar_location(dir_from, dir_to):
     return tar_filename
 
 
-def check_archive_contents(tar_filename):
-    tar_member_names = set()
-
+def check_archive(tar_filename, included_files):
+    # type: (str, set) -> bool
+    """
+    Check the contents of the tar against the original files.
+    Check no files are missing in the archive.
+    :param tar_filename: The archive to check.
+    :param included_files: The list of files the archive should contain.
+    :return True if archive is ok
+    """
     print '\nChecking tar: [{}]'.format(tar_filename)
 
-    # todo use getmembers() and iterate over the files.  Compare the agains the real file.
+    good_archive = True
+    if not match_members_with_fs(tar_filename):
+        good_archive = False
+    if not match_fs_with_members(tar_filename, included_files):
+        good_archive = False
+
+    return good_archive
+
+
+def match_fs_with_members(tar_filename, included_files):
+    # type: (str, set) -> bool
+    """
+    Check the tar contains a file match for every one on the filesystem.
+    :param tar_filename: The archive to check.
+    :param included_files: The names of the expected files from the filesystem.
+    :return True if archive is ok
+    """
+    good_archive = True
 
     with tarfile.open(tar_filename, "r") as tar:
+        member_fs_names = set()
         for member in tar.getmembers():
-            f = tar.extractfile(member)
-            content = f.read()
+            member_name_fs = os.path.join('/', member.name)
+            member_fs_names.add(member_name_fs)
 
-            member_name = os.path.join('/', member.name)
+        for fs_name in included_files:
+            if not os.path.abspath(fs_name) in member_fs_names:
+                print 'MISSING FROM TAR [{}]'.format(fs_name)
+                good_archive = False
 
-            if os.path.isfile(member_name):
-                print 'Original file found: [{}]'.format(member_name)
+    return good_archive
+
+
+def match_members_with_fs(tar_filename):
+    # type: (str) -> bool
+    """
+    Read each file in the archive and deep match agains the original on the filesystem.
+    :param tar_filename: The archive to check.
+    :return: True if the archive is ok
+    """
+    good_archive = True
+
+    with tarfile.open(tar_filename, "r") as tar:
+
+        for member in tar.getmembers():
+            member_name_fs = os.path.join('/', member.name)
+
+            if os.path.isfile(member_name_fs):
+                member_as_file = tar.extractfile(member)
+                with open(member_name_fs) as original_file:
+                    if member_as_file.read() == original_file.read():
+                        print 'MATCHED {}'.format(member_name_fs)
+                    else:
+                        print 'CORRUPT {}'.format(member_name_fs)
+                        good_archive = False
             else:
-                print 'Original file missing: [{}]'.format(member_name)
+                print 'MISSING FROM  FS [{}]'.format(member_name_fs)
+                good_archive = False
 
-                # figure out how to compare content with original file.
-
-    return
+    return good_archive
 
 
-#
-# Create a clean archive.  Process incoming args.
 def main(argv):
+    # type: (list) -> None
+    """
+    Create a clean archive.  Process incoming args.
+    :param argv: Program arguments.
+    """
     flags = Flags()
 
     dir_from = ''
     dir_to = _dir_to_default
+
+    skipped_files = set()
+    included_files = set()
 
     if (argv is None) or (len(argv) == 0):
         print_help()
@@ -243,9 +338,12 @@ def main(argv):
 
         if opt in ("-i", "--input-dir"):
             dir_from = arg
-            list_tree(src=dir_from, ignore=shutil.ignore_patterns(*IGNORE_PATTERNS))
+            list_tree(src=dir_from
+                      , included_files=included_files
+                      , skipped_files=skipped_files
+                      , ignore=shutil.ignore_patterns(*IGNORE_PATTERNS))
 
-            if len(_included_files) == 0:
+            if len(included_files) == 0:
                 print('\n** No files found to archive! **')
                 sys.exit(1)
 
@@ -262,10 +360,10 @@ def main(argv):
             flags.check_contents = True
 
     if flags.list_only:
-        print_list(_included_files, _ignored_files)
+        print_list(included_files, skipped_files)
 
     if flags.summary:
-        print_summary(_included_files, _ignored_files)
+        print_summary(included_files, skipped_files)
 
     tar_filename = build_tar_location(dir_from, dir_to)
 
@@ -273,10 +371,12 @@ def main(argv):
         print 'Archive to: [{}]\n'.format(tar_filename)
 
     if not flags.list_only:
-        create_archive(tar_filename, _included_files, flags)
+        create_archive(tar_filename, included_files, flags)
 
     if flags.check_contents:
-        check_archive_contents(tar_filename)
+        if not check_archive(tar_filename, included_files):
+            sys.exit("** Archive Matching Error: Check report! **")
+        print '\n** Archive Validation Success! **'
 
     sys.exit(0)
 
